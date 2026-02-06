09c03a019f9e3d98a3fb91551bc52c1efcbeca64
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2026-02-06 13:50:58 +0100

    edificio e tipoPostazione

--- GestionePrenotazioni/src/main/java/it/gestioneprenotazioni/model/Edificio.java
+++ GestionePrenotazioni/src/main/java/it/gestioneprenotazioni/model/Edificio.java
@@ -1,4 +1,39 @@
 package it.gestioneprenotazioni.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity
 public class Edificio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String indirizzo;
+    private String citta;
+
+    public Edificio() {
+    }
+    public Edificio(String nome, String indirizzo, String citta) {
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.citta = citta;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
 }
---
+++ GestionePrenotazioni/src/main/java/it/gestioneprenotazioni/model/TipoPostazione.java
@@ -0,0 +1,7 @@
+package it.gestioneprenotazioni.model;
+
+public enum TipoPostazione {
+    PRIVATO,
+    OPENSPACE,
+    SALA_RIUNIONI
+}
